79d4cba605759dfabf719370186c0093781a15be
HiruDuce <hieuphung180805@gmail.com>
2025-10-19 18:01:09 +0700

    Add DB test file with MySQLi

--- test_db.php
+++ test_db.php
@@ -1,14 +1,22 @@
 <?php
 $host = getenv("MYSQLHOST");
+$db   = getenv("MYSQLDATABASE");
 $user = getenv("MYSQLUSER");
 $pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT");
 
+// Kết nối MySQLi
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("Kết nối thất bại: " . $conn->connect_error);
-} 
-echo "Kết nối DB thành công!";
+    die("Kết nối DB thất bại: " . $conn->connect_error);
+} else {
+    echo "Kết nối DB thành công với MySQLi!";
+}
+
+// Kiểm tra các bảng
+$result = $conn->query("SHOW TABLES");
+while($row = $result->fetch_array()) {
+    echo $row[0] . "<br>";
+}
 ?>
