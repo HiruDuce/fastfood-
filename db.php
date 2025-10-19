9a4e367f4ea1f637a445023c476f055b77f7ad9a
HiruDuce <hieuphung180805@gmail.com>
2025-10-19 15:25:55 +0700

    Final DB configuration for Railway

--- db.php
+++ db.php
@@ -1,10 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root"; // mặc định XAMPP
-$pass = "";     // mật khẩu trống
-$db   = "fastfood_db";
+// Sử dụng hàm getenv() để lấy các biến môi trường do Railway cung cấp
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT"); // Thêm Port vì Railway không dùng Port mặc định 3306
 
-$conn = new mysqli($host, $user, $pass, $db);
+// Khởi tạo kết nối mới
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
